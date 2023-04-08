3c2ee48c32e18369d904ad1ba218990761df4612
mehmet eren sonmez <merensnmz@gmail.com>
2023-04-08 16:40:46 +0300

    Gradle added and files format changed

---
+++ src/main/java/dictionary/ce216team5_04_6_last/Main.java
@@ -0,0 +1,22 @@
+package dictionary.ce216team5_04_6_last;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("CE216-TEAM5");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dictionary.ce216team5_04_6_last{
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens dictionary.ce216team5_04_6_last to javafx.fxml;
+    exports dictionary.ce216team5_04_6_last;
+}
