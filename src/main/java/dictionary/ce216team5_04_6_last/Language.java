75936086804e3f6a9cb93cc0d622e67dc7752677
beyzaaltuner <beyzaaltuner42@gmail.com>
2023-04-08 18:10:49 +0300

    loadWordsFromFiles is updated to work with all file formats

--- src/main/java/dictionary/ce216team5_04_6_last/Language.java
+++ src/main/java/dictionary/ce216team5_04_6_last/Language.java
@@ -1,20 +1,32 @@
 package dictionary.ce216team5_04_6_last;
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.Charset;
+import java.util.*;
 
 
 public class Language {
 
     private String srcLang;
     private String trgLang;
-    private Map<String, String> hashMap;
+    private Map<String, List<String>> hashMap;
+    private HashMap<String, ArrayList<String>> dictionary;
 
-    public Map<String, String> getHashMap() {
+    public Language(String text, String translation) {
+    }
+
+    public HashMap<String, ArrayList<String>> getDictionary() {
+        return dictionary;
+    }
+    public void setDictionary(HashMap<String, ArrayList<String>> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+
+    public Map<String, List<String>> getHashMap() {
         return hashMap;
     }
 
-    public void setHashMap(Map<String, String> hashMap) {
+    public void setHashMap(Map<String, List<String>> hashMap) {
         this.hashMap = hashMap;
     }
 
@@ -42,45 +54,53 @@ public class Language {
         this.trgLang = trgLang;
     }
 
-
-    //loadWordsFromFile method to read from a file and put the words and translations to related hashmap
-    // main usage of the method is to find how to read from a file and put it to a hashmap
-
-    public void loadWordsFromFile(String filePath) {
+    public void loadWordsFromFile(String filePath, Charset charset) {
         System.out.println("loadWordsFromFile");
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            String text = sb.toString();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath,charset))) {
 
-            String[] lines = text.split("\n");
             hashMap = new HashMap<>();
 
-            for (int i = 0; i < lines.length; i++) {
-                String string = lines[i].trim();
-                if (string.endsWith("/")) {
-                    String[] parts = string.split("/");
-                    String word = parts[0].trim();
-                    String translations = "";
-                    for (int j = i+1; j < lines.length; j++) {
-                        String nextLine = lines[j].trim();
-                        if (nextLine.isEmpty()) {
-                            continue;
-                        } else if (nextLine.endsWith("/")) {
-                            break;
-                        } else {
-                            translations += nextLine.trim() + " ";
-                        }
+            String line;
+            String currentWord = null;
+            List<String> translations = new ArrayList<>();
+
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("<")) {
+                    if (currentWord != null && !translations.isEmpty()) {
+                        hashMap.put(currentWord, translations);
+                        translations = new ArrayList<>();
                     }
-                    // add word and its translations to the HashMap
-                    hashMap.put(word, translations);
-                    System.out.println(word);
-                    System.out.println(translations);
+
+                    int bracketIndex = line.indexOf("<");
+                    int slashIndex = line.indexOf("/");
+
+                    if (slashIndex != -1 && slashIndex < bracketIndex) {
+                        String[] parts = line.split("/");
+                        currentWord = parts[0].trim();
+                    } else {
+                        String[] parts = line.split(" ");
+                        currentWord = parts[0].trim();
+                    }
+                } else if (line.contains("/")) {
+                    if (currentWord != null && !translations.isEmpty()) {
+                        hashMap.put(currentWord, translations);
+                        translations = new ArrayList<>();
+                    }
+
+                    String[] parts = line.split("/");
+                    currentWord = parts[0].trim();
+                } else if (!line.trim().isEmpty()) {
+                    translations.add(line.trim());
                 }
+            }
+
+            if (currentWord != null && !translations.isEmpty()) {
+                hashMap.put(currentWord, translations);
+            }
+
+            for (Map.Entry<String, List<String>> entry : hashMap.entrySet()) {
+                System.out.println(entry.getKey() + " -> " + entry.getValue());
             }
         } catch (IOException e) {
             e.printStackTrace();
