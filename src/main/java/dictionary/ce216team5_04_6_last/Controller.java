a515c370d18da8805aaf8f794e30c3a83cf4041a
gokcearslan <gokcearslann17@gmail.com>
2023-04-08 18:18:41 +0300

    updated version of search method (for all files)

--- src/main/java/dictionary/ce216team5_04_6_last/Controller.java
+++ src/main/java/dictionary/ce216team5_04_6_last/Controller.java
@@ -1,73 +1,75 @@
 package dictionary.ce216team5_04_6_last;
-    import javafx.collections.FXCollections;
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
-    public class Controller implements Initializable {
+public class Controller implements Initializable {
 
-        @FXML
-        private TableColumn<Language, String> column1 = new TableColumn<>();
+    @FXML
+    private TableColumn<Language, String> Translation = new TableColumn<>();
+    @FXML
+    private TableView<Language> sourceLangTable = new TableView<>();
+    @FXML
+    private TextField searchBox;
+    @FXML
+    private ChoiceBox SourceCB=new ChoiceBox<>();
+    @FXML
+    private ChoiceBox TargetCB=new ChoiceBox<>();
 
-        @FXML
-        private TableColumn<Language, String> column2 = new TableColumn<>();
+    public void search() {
+        String sourceLang= (String) SourceCB.getValue();
+        String targetLang= (String) TargetCB.getValue();
+        String srcSource = "src/main/resources/dictionary/ce216team5_04_6_last/";
+        String srcTxt = ".txt";
+        String filePath= srcSource +sourceLang+targetLang+srcTxt;
 
-        @FXML
-        private TableView<Language> table = new TableView<>();
+        Language language = new Language();
+        System.out.println("search");
+
+        language.loadWordsFromFile(filePath,StandardCharsets.UTF_8);
 
 
-        @FXML
-        private TextField sourceLang;
+        List<String> values = null;
 
-
-
-
-
-        public void search() {
-            String filePath = getClass().getResource("tur-eng.txt").getPath();
-            Language language = new Language();
-            System.out.println("search");
-
-            // Load the words into the hashMap using threads
-            language.loadWordsFromFile(filePath);
-
-            // Get the translation for the input word from the hashMap
-            String value = null;
-            for (String word : language.getHashMap().keySet()) {
-                if (word.equalsIgnoreCase(sourceLang.getText())) {
-                    value = language.getHashMap().get(word);
-                    break;
-                }
-            }
-
-            // Create a new observable list with the translation and set it to the table
-            if (value != null) {
-                ObservableList<Language> list = FXCollections.observableArrayList(new Language(value));
-                table.setItems(list);
-            } else {
-                table.getItems().clear();
+        for (String word : language.getHashMap().keySet()) {
+            if (word.equalsIgnoreCase(searchBox.getText())) {
+                values = language.getHashMap().get(word);
+                break;
             }
         }
 
 
-
-
-
-        @Override
-        public void initialize(URL url, ResourceBundle resourceBundle) {
-
-            column1.setCellValueFactory(new PropertyValueFactory<Language, String>("trgLang"));
-
-
-
-
+        if (values != null && !values.isEmpty()) {
+            ObservableList<Language> list = FXCollections.observableArrayList();
+            for (String value : values) {
+                list.add(new Language(value));
+            }
+            sourceLangTable.setItems(list);
+        } else {
+            sourceLangTable.getItems().clear();
         }
     }
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Translation.setCellValueFactory(new PropertyValueFactory<Language, String>("trgLang"));
+        SourceCB.getItems().addAll("English", "French", "German","Italian", "Greek", "Turkish");
+        TargetCB.getItems().addAll("English", "French", "German","Italian", "Greek", "Turkish");
+
+
+
+
+    }
+}
